593ff002d4fa5e6c5281d99592e3eeaf4aaf3757
taoquan00001 <zhangtaoquan@qq.com>
2019-01-05 13:46:54 +0800

    Add files via upload

---
+++ configuration.php
@@ -0,0 +1,10 @@
+<?php
+define('INSTALLED', 1);
+define('SVMC_VERSION', '3.2 Beta');
+
+$config['dbHost'] = 'localhost';
+$config['dbUser'] = 'SolusVMControll';
+$config['dbPass'] = 'cnHiGMjfTWiFPkAT';
+$config['dbName'] = 'SolusVMControll';
+$config['language'] = 'en-us';
+?>
